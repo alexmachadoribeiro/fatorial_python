91c6246520bd05c22380b10e87c1f3927f05db05
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2024-06-05 19:52:38 -0300

    V 1.0

---
+++ main.py
@@ -0,0 +1,12 @@
+# função fatorial
+def calcular_fatorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * calcular_fatorial(n - 1)
+    
+# programa principal
+n = int(input('Informe um número inteiro: '))
+
+# exibe o resultado do fatorial
+print(f'Fatorial de {n}: {calcular_fatorial(n)}.')
